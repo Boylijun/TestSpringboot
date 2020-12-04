0c21aad98ef5427a59be8eb8cbd0960245c42b4b
QQ512433465 <mima512433465>
2020-12-04 14:51:32 +0800

    增加类

---
+++ src/main/java/com/yzl/demo1/controller/helController.java
@@ -0,0 +1,29 @@
+package com.yzl.demo1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class helController {
+
+    @RequestMapping("/index")
+    public String sayHello(){
+        return "index";
+    }
+    @RequestMapping("/index1")
+    public String sayHello1(){
+
+        return "index1";
+    }
+    @RequestMapping("/index2")
+    public String sayHello2(){
+
+        return "index2";
+    }
+    @RequestMapping("/index4")
+    public String sayHello3(){
+
+        return "index4";
+    }
+
+}
